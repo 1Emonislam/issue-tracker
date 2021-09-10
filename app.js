2af8ce230481753464262baf213dfac8ba0da62f
1Emonislam <emonislamweb@gmail.com>
2021-09-10 21:47:07 +0600

    close issue function added

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// submitIssue button
 document
 	.getElementById('issueInputForm')
 	.addEventListener('submit', submitIssue);
@@ -22,3 +23,34 @@ function submitIssue(e) {
 	fetchIssues();
 	e.preventDefault();
 }
+
+//load data local storage
+const fetchIssues = () => {
+	const issues = JSON.parse(localStorage.getItem('issues'));
+	const issuesList = document.getElementById('issuesList');
+	issuesList.innerHTML = '';
+
+	for (var i = 0; i < issues.length; i++) {
+		const { id, description, severity, assignedTo, status } = issues[i];
+
+		issuesList.innerHTML += `<div class="well">
+                              <h6>Issue ID: ${id} </h6>
+                              <p><span class="label label-info"> ${status} </span></p>
+                              <h3> ${description} </h3>
+                              <p><span class="glyphicon glyphicon-time"></span> ${severity}</p>
+                              <p><span class="glyphicon glyphicon-user"></span> ${assignedTo}</p>
+                              <a href="#" onclick="closeIssue(${id})" class="btn btn-warning">Close</a>
+                              <a href="#" onclick="deleteIssue(event,${id})" class="btn btn-danger">Delete</a>
+                              </div>`;
+	}
+};
+
+// close issue
+const closeIssue = id => {
+	const issues = JSON.parse(localStorage.getItem('issues'));
+	const currentIssue = issues.find(issue => parseInt(issue.id) === id);
+	// console.log(currentIssue);
+	currentIssue.status = 'Closed';
+	localStorage.setItem('issues', JSON.stringify(issues));
+	fetchIssues();
+};
